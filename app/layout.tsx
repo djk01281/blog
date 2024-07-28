7a7641718aa39e5b7bbc82191d4605064746156c
djk01281 <djk01281@gmail.com>
2024-07-28 16:39:01 +0900

    feat: create top portion of the landing page

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,15 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+import localFont from "next/font/local";
 import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
+const pretendard = localFont({
+  src: "../public/PretendardVariable.woff2",
+  display: "swap",
+  weight: "45 920",
+  variable: "--font-pretendadrd",
+});
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -16,7 +23,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={pretendard.className}>{children}</body>
     </html>
   );
 }
