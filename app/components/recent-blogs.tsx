820691ed2259d1699de816e2c139b02a7d297e45
djk01281 <djk01281@gmail.com>
2024-08-25 14:43:23 +0900

    style: fix recent cards paddings

--- app/components/recent-blogs.tsx
+++ app/components/recent-blogs.tsx
@@ -6,7 +6,7 @@ export default async function RecentBlogs() {
   const blogs = await getAllBlogs();
 
   return (
-    <div className="w-full flex flex-col gap-6 px-8">
+    <div className="w-full flex flex-col gap-6 ">
       <div className="w-full grid grid-cols-1 md:grid-cols-2 gap-8 md:gap-12 ">
         {blogs.map(
           (blog, index) =>
@@ -18,7 +18,7 @@ export default async function RecentBlogs() {
               tags: blog.tags,
               slug: blog.slug,
               cover: blog.cover,
-              className: "bg-[#2d2929] rounded-2xl",
+              className: "bg-[#2d2929] rounded-2xl ",
             })
         )}
       </div>
