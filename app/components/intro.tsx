4077f0c7704110a15c35517fd8a12c52328228ed
djk01281 <djk01281@gmail.com>
2024-09-08 00:12:09 +0900

    style: shorten intro animation duration

--- app/components/intro.tsx
+++ app/components/intro.tsx
@@ -42,7 +42,7 @@ export default function Intro() {
         <motion.div
           initial={{ opacity: 0, y: 48 }}
           animate={{ opacity: 1, y: 0 }}
-          transition={{ duration: 1, delay: 0.5 }}
+          transition={{ duration: 0.75 }}
           className="w-full flex gap-2 items-center flex-col text-center"
         >
           <h1 className="text-3xl sm:text-4xl md:text-5xl lg:text-6xl font-extrabold  font-pretendard">
